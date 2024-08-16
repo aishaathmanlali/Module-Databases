aa0c838a4acecbde8bf51985ff99e6bf7b675417
aishaathmanlali <aishaathmanal@gmail.com>
2024-08-16 16:21:57 +0100

    add gitignore file

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+import pg from "pg";
+const { Pool } = require("pg");
+
+
+
 // Your code to run the server should go here
 // Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
 // Use environment variables instead:
 // https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
 
-module.exports = app;
+// module.exports = app;
+app.use(express.json());
+
+const db = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+
